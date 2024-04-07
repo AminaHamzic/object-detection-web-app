715bae059c72e9a7709cb892d0de7487ac2a26b3
Admin@DESKTOP-EQ55Q8H <amina.hamzic@stu.ibu.edu.ba>
2024-04-07 22:28:27 +0200

    s3 bucket

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 import boto3
-from fastapi import FastAPI, UploadFile, File, Depends
+from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
-from botocore.exceptions import NoCredentialsError
-from starlette.responses import JSONResponse
+
 
 app = FastAPI()
 s3_client = boto3.client('s3')
@@ -24,16 +23,7 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
-@app.post("/uploadfile/")
-async def upload_file_to_s3(file: UploadFile = File(...)):
-    file_contents = await file.read()
-    try:
-        s3_client.put_object(
-            Bucket="uplodefilebucket",
-            Key=file.filename,
-            Body=file_contents
-        )
-        return JSONResponse(content={"message": "Upload successful"}, status_code=200)
-    except Exception as e:
-        # Handle exception
-        return JSONResponse(content={"message": str(e)}, status_code=500)
+@app.post("/upload/")
+async def upload(file: UploadFile | None = None):
+    if not file:
+        raise HTTPException(status_code=400, detail="No file")
--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -3,24 +3,3 @@ $(function () {
     feather.replace();
 });
 
-document.getElementById('upload_button').addEventListener('click', async () => {
-    let fileInput = document.getElementById('file_upload');
-    let formData = new FormData();
-    formData.append('file_upload', fileInput.files[0]);
-
-    try {
-        let response = await fetch('http://127.0.0.1:8000/uploadfile/', {
-            method: 'POST',
-            body: formData
-        });
-
-        if (response.ok) {
-            let data = await response.json();
-            console.log('Upload successful', data);
-        } else {
-            console.error('Upload failed');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-});
